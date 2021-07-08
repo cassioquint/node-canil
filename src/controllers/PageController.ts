b04f8ae83927b671fae14d4f0cdeb4778d434aaf
Cássio Quintana <cassioquint@gmail.com>
2021-07-07 22:41:44 -0300

    final project

--- src/controllers/PageController.ts
+++ src/controllers/PageController.ts
@@ -1,15 +1,52 @@
 import { Request, Response } from 'express';
+import { Pet } from '../models/Pet';
+import { createMenuObject } from '../helpers/createMenuObject';
 
 export const home = (req: Request, res: Response) => {
-    res.send('Home controller');
-    //res.render('pages/page');
+    let petList = Pet.getAll();
+
+    res.render('pages/page', {
+        menu: createMenuObject('all'),
+        banner: {
+            title: 'Todos os animais',
+            background: 'allanimals.jpg'
+        },
+        petList
+    });
 };
 export const dogs = (req: Request, res: Response) => {
-    //res.render('pages/page');
+    let petList = Pet.getFromType('dog');
+
+    res.render('pages/page', {
+        menu: createMenuObject('dog'),
+        banner: {
+            title: 'Cachorros',
+            background: 'banner_dog.jpg'
+        },
+        petList
+    });
 };
 export const cats = (req: Request, res: Response) => {
-    //res.render('pages/page');
+    let petList = Pet.getFromType('cat');
+
+    res.render('pages/page', {
+        menu: createMenuObject('cat'),
+        banner: {
+            title: 'Gatos',
+            background: 'banner_cat.jpg'
+        },
+        petList
+    });
 };
 export const fishes = (req: Request, res: Response) => {
-    //res.render('pages/page');
+    let petList = Pet.getFromType('fish');
+
+    res.render('pages/page', {
+        menu: createMenuObject('fish'),
+        banner: {
+            title: 'Peixes',
+            background: 'banner_fish.jpg'
+        },
+        petList
+    });
 };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import * as PageController from '../controllers/PageController';
-import * as SearchController from '../controllers/searchController';
+import * as SearchController from '../controllers/SearchController';
 
 const router = Router();
 
@@ -8,7 +8,6 @@ router.get('/', PageController.home);
 router.get('/dogs', PageController.dogs);
 router.get('/cats', PageController.cats);
 router.get('/fishes', PageController.fishes);
-
 router.get('/search', SearchController.search);
 
 export default router;
